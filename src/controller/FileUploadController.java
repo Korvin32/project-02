f390c5632f9ba98ac896ce85d3de21b549307292
Kostyantyn Zagorodniy <korvin32@yahoo.com>
2016-05-28 12:44:16 +0200

    Primefaces based file upload added with controller

---
+++ src/controller/FileUploadController.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ManagedBean
+@SessionScoped
+public class FileUploadController implements Serializable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FileUploadController.class);
+    
+    private static final long serialVersionUID = 1L;
+
+    public void fileUploadHandler(FileUploadEvent event) {
+        UploadedFile uFile = event.getFile();
+        String uFileName = uFile.getFileName();
+        LOG.info("--- fileUploadHandler(): upload file name is '{}'", uFileName);
+        try {
+            InputStream iStrem = uFile.getInputstream();
+            
+            Path newFile = Paths.get("D:/temp/_upload", uFileName);
+            
+            Files.copy(iStrem, newFile, StandardCopyOption.REPLACE_EXISTING);
+            
+            FacesMessage message = new FacesMessage("Succesful", uFile.getFileName() + " is uploaded.");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+            
+            iStrem.close();
+        } catch (IOException e) {
+            LOG.error("Error: " + e.getMessage(), e);
+        }
+        LOG.info("--- fileUploadHandler(): upload file done!");
+    }
+}
